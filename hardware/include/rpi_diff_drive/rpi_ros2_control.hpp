04f96b2a46a41b896111f4289e77c55b4a7d5c79
Dharan Kumar Nallagatla <84310855+Dharan-kumar@users.noreply.github.com>
2024-05-27 11:36:39 +0530

    Update rpi_ros2_control.hpp

--- hardware/include/rpi_diff_drive/rpi_ros2_control.hpp
+++ hardware/include/rpi_diff_drive/rpi_ros2_control.hpp
@@ -42,34 +42,26 @@ namespace rpi_diff_drive
         Params m_Params;
             
         public:
-            //RCLCPP_SHARED_PTR_DEFINITIONS(RpiController)
             RpiController();
 
             hardware_interface::CallbackReturn on_init(
             const hardware_interface::HardwareInfo & info) override;
 
-        
             std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
-
             
             std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
-
-            
+   
             hardware_interface::CallbackReturn on_activate(
                 const rclcpp_lifecycle::State & previous_state) override;
 
-            
             hardware_interface::CallbackReturn on_deactivate(
                 const rclcpp_lifecycle::State & previous_state) override;
 
-            
             hardware_interface::return_type read(
                 const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-            
             hardware_interface::return_type write(
                 const rclcpp::Time & time, const rclcpp::Duration & period) override;
-
 
             rclcpp::Logger logger_;
 
@@ -78,7 +70,6 @@ namespace rpi_diff_drive
             std::vector<double> hw_commands_;
             std::vector<double> hw_positions_;
             std::vector<double> hw_velocities_;
-
     };
 }
 #endif
